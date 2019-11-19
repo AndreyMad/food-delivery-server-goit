d157e37d26f9f59bc5edc106476756055c0eca77
AndreyMad <Andrey.mad.87@gmail.com>
2019-11-19 02:16:42 +0200

    h1 done

---
+++ src/routes/signUp.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+const validator = require('validator')
+const signUpRoute=async (req,res) =>{
+if(req.method !=='POST'){ 
+    console.log('wrong method');
+return;
+}else{
+    let body = "";
+    await req.on("data", function(data) {
+      body += data;
+    });
+    const user = JSON.parse(body);
+
+    //validate reqData
+    const isValidReqData = !validator.isEmpty(user.username)&&
+    validator.isMobilePhone(user.telephone)&&
+    validator.isByteLength(user.password,{min:5})&&
+    validator.isEmail(user.email);
+    
+    if(!isValidReqData){
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.write("Error: Bad Request");
+        res.end();
+        return;
+    }
+        const filePath = path.join(
+        __dirname,
+        "../../",
+        "src/",
+        "db/",
+        "users/",
+        `${user.username}.json`
+      );
+        fs.writeFile(filePath, body, err => {
+        if (err) {
+          throw err;
+        };
+        const userBody = {
+          status: "success",
+          user: JSON.parse(body)
+        };
+        response.writeHead(201, { "Content-Type": "application/json" });
+        response.write(JSON.stringify(userBody));
+        response.end();
+      });
+    }
+
+
+}
+module.exports =signUpRoute;
